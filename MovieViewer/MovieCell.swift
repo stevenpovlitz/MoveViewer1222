a8d4da17f429aea4ecd2cd0bcfeed8aca267a716
Steven Povlitz <steven.povlitz@gmail.com>
2016-01-25 23:47:28 -0500

    its working pt 4 done

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Steven Povlitz on 1/25/16.
+//  Copyright © 2016 Steven Povlitz. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
